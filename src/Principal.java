c3cda7f47626ed8d0790e20522aa6fe93b9130f6
JefersonROE <jefersonrodriguesoe@gmail.com>
2024-04-02 13:04:15 -0300

    Criando um projeto aplicando Orientação a Objetos.

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+    }
+}
