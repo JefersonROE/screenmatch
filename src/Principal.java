587d51df4dbcab0873cc7cedd033d1d187bbba7b
JefersonROE <jefersonrodriguesoe@gmail.com>
2024-04-25 12:51:11 -0300

    Criando um projeto aplicando Orientação a Objetos.

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -43,5 +44,11 @@ public class Principal {
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
         filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVizualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
