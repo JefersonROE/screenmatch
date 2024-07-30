9655d257bb66133e65376050e54305c91b565374
JefersonROE <jefersonrodriguesoe@gmail.com>
2024-07-30 13:37:51 -0300

    Criando um projeto aplicando Orientação a Objetos.

--- src/Principal.java
+++ src/Principal.java
@@ -65,6 +65,8 @@ public class Principal {
         listaDeFilmes.add(outroFilme);
         System.out.println("Tamanho da lista: " + listaDeFilmes.size());
         System.out.println("Primeiro Filme: " +  listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+
 
 
 
